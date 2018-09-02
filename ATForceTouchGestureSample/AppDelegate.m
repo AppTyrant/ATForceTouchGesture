be03eb9d299de0a13fd0dc1a5f33612b5235244c
AppTyrant <>
2018-09-02 09:25:01 -0400

    setting up sample code.

--- ATForceTouchGestureSample/AppDelegate.m
+++ ATForceTouchGestureSample/AppDelegate.m
@@ -7,22 +7,84 @@
 //
 
 #import "AppDelegate.h"
+#import "ATForceTouchGesture.h"
+#import "NSTextField+HasFocusProperty.h"
+#import "NSView+CenterProp.h"
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *label;
+
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+-(void)applicationDidFinishLaunching:(NSNotification*)aNotification
+{
     // Insert code here to initialize your application
+    ATForceTouchGesture *forceTouchGesture = [[ATForceTouchGesture alloc]initWithTarget:self action:@selector(startEditingLabelAction:)];
+    [self.label addGestureRecognizer:forceTouchGesture];
 }
 
-
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+-(void)startEditingLabelAction:(ATForceTouchGesture*)sender
+{
+    if (self.label.doesTextFieldHaveFocus)
+    {
+        NSLog(@"Ignore pressure event. Text field already editing.");
+        return;
+    }
+    
+    switch (sender.state)
+    {
+        case NSGestureRecognizerStateBegan:
+        case NSGestureRecognizerStateChanged:
+        {
+            self.label.backgroundColor = NSColor.alternateSelectedControlColor;
+            self.label.textColor = NSColor.whiteColor;
+        }
+        break;
+            
+        case NSGestureRecognizerStateRecognized:
+        {
+            self.label.backgroundColor = NSColor.windowBackgroundColor;
+            self.label.textColor = NSColor.labelColor;
+            
+            self.label.editable = YES;
+            [self.window makeFirstResponder:self.label];
+        }
+        break;
+            
+        case NSGestureRecognizerStateCancelled:
+        {
+            NSLog(@"Cancelled, reset label's appearance.");
+            self.label.backgroundColor = NSColor.windowBackgroundColor;
+            self.label.textColor = NSColor.labelColor;
+        }
+        break;
+            
+        default:
+        {
+            NSLog(@"Gesture state: %li",sender.state);
+        }
+        break;
+    }
 }
 
+#define LABEL_MIN_WIDTH 50.0
+-(IBAction)windowBackgroundClicked:(NSClickGestureRecognizer*)sender
+{
+    CGPoint currentCenter = self.label.center;
+    self.label.editable = NO;
+    [self.window makeFirstResponder:nil];
+    [self.label sizeToFit];
+    if (self.label.frame.size.width < LABEL_MIN_WIDTH)
+    {
+        CGRect adjustedFrame = self.label.frame;
+        adjustedFrame.size.width = LABEL_MIN_WIDTH;
+        self.label.frame = adjustedFrame;
+    }
+    self.label.center = currentCenter;
+}
 
 @end
